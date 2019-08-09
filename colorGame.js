03e5156a18e573f9a86463c23e7daa96b4f674f9
Michael Anokye <michaelanokyej@yahoo.com>
2019-08-08 22:08:22 -0400

    added logic to fade out clicked boxes to background color if its not picked color

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,25 @@ let colors = [
 ];
 
 let squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+let colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
 
 for(let i = 0; i < squares.length; i++){
+    // add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    // add click eventListener 
+    squares[i].addEventListener("click", function(){
+        // grab color of clicked square and compare color to picked color
+      let clickedColor = this.style.backgroundColor;
+      if(clickedColor === pickedColor){
+          document.body.style.backgroundColor = pickedColor;
+      }
+      else{
+          this.style.backgroundColor = "#232323";
+      }
+    });
 }
